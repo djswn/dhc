f143598d5f52fa68dc029dcffe225f4edc5a31fd
djswn <cej0836@naver.com>
2024-06-26 23:38:27 +0900

    Update client.js

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 const startBtn = document.getElementById('start-btn');
 const resultDiv = document.getElementById('result');
-const apiUrl = 'http://127.0.0.1:5000/api/process_input'; // 서버 주소를 설정합니다.
+const apiUrl = 'https://djswn.github.io/dhc/chat.html'; // 서버 주소를 설정합니다.
 
 startBtn.addEventListener('click', async () => {
   const recognition = new webkitSpeechRecognition();
